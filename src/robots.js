41b7742291bdb14b909aafb20b38a04be2b878b2
Marius Drilea <marius.drilea2016@gmail.com>
2020-10-03 18:44:26 +0300

    commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Marius Drilea',
+        username: 'marius',
+        email: 'marius.drilea2016@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Paty Pirlog',
+        username: 'paty',
+        email: 'paty.pirlog@yahoo.com'
+    },
+    {
+        id: 3,
+        name: 'Fha',
+        username: 'fha',
+        email: 'fha@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Marius Drilea2',
+        username: 'marius2',
+        email: 'marius2.drilea2016@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Marius Drilea3',
+        username: 'marius3',
+        email: 'marius3.drilea2016@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Marius Drilea4',
+        username: 'marius4',
+        email: 'marius4.drilea2016@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Marius Drilea5',
+        username: 'marius5',
+        email: 'marius5.drilea2016@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Marius Drilea6',
+        username: 'marius6',
+        email: 'marius6.drilea2016@gmail.com'
+    }
+]
