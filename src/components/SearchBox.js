12f5440ca622448f39104ab38d8ea39b2e5440be
Marius Drilea <marius.drilea2016@gmail.com>
2020-10-05 12:48:12 +0600

    improvement in robots application

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({searchfield, searchChange}) => {
             <input
             className = "pa3 ba b--green bg-lightest-blue"
             type='search' 
-            placeholder = 'ssearch robot' 
+            placeholder = 'Search robot' 
             onChange={searchChange}
             />
         </div>
